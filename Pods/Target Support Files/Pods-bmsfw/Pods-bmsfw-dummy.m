0a16f16a71ba660b43deacb6705c192e73deb6d7
Chaiyaporn Suratemeekul <manoi_rx@yahoo.com>
2017-08-16 08:48:31 +0700

    update file

---
+++ Pods/Target Support Files/Pods-bmsfw/Pods-bmsfw-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bmsfw : NSObject
+@end
+@implementation PodsDummy_Pods_bmsfw
+@end
--- bmsfw/bmsfw.h
+++ bmsfw/bmsfw.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <BMSRTCLib/BMSRTCLib.h>
 
 //! Project version number for bmsfw.
 FOUNDATION_EXPORT double bmsfwVersionNumber;
