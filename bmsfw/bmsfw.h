4c3197c3a8de0b95381c6fbfd2745d44a35104b2
Chaiyaporn Suratemeekul <manoi_rx@yahoo.com>
2017-08-16 08:36:52 +0700

    update file

---
+++ bmsfw/bmsfw.h
@@ -0,0 +1,19 @@
+//
+//  bmsfw.h
+//  bmsfw
+//
+//  Created by Chaiyaporn Suratemeekul on 8/16/2560 BE.
+//  Copyright © 2560 Chaiyaporn Suratemeekul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for bmsfw.
+FOUNDATION_EXPORT double bmsfwVersionNumber;
+
+//! Project version string for bmsfw.
+FOUNDATION_EXPORT const unsigned char bmsfwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bmsfw/PublicHeader.h>
+
+
